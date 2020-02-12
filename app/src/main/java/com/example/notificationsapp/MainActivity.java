d2cd0cb49a30d44745f14a3bae23f265a3614006
admin <admin@admins-iMac.local>
2020-02-12 13:51:29 +0200

    Initial commit

--- app/src/main/java/com/example/notificationsapp/MainActivity.java
+++ app/src/main/java/com/example/notificationsapp/MainActivity.java
@@ -3,12 +3,59 @@ package com.example.notificationsapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String CHANNEL_ID_1 = "notification_channel_1";
+    public static final String CHANNEL_ID_2 = "notification_channel_2";
+
+    private EditText titleEditText;
+    private EditText messageEditText;
+    private Button notifyButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        titleEditText = findViewById(R.id.edtTitle);
+        messageEditText = findViewById(R.id.edtMessage);
+        notifyButton = findViewById(R.id.btnNotify);
+
+        notifyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                displayNotification();
+            }
+        });
+
     }
+
+    private void displayNotification() {
+        String title = titleEditText.getText().toString();
+        String message = messageEditText.getText().toString();
+
+        if (title.isEmpty() || message.isEmpty()) {
+            Toast.makeText(this, "Notification incomplete!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // TODO - Display notification type
+
+    }
+
+    private void displayNormalNotification(String title, String message) {
+        // TODO - Create and show normal text notification
+
+    }
+
+    private void displayBigPictureNotification(String tile, String message) {
+        // TODO - Create and show picture notification
+
+    }
+
 }
